93588de35c562450f16ed3e53c2dee84a92fcaa1
Alex Hamill <alexander.hamill@icloud.com>
2026-01-08 13:08:43 -0500

    more tests

--- bigint/BIGINTTD.c
+++ bigint/BIGINTTD.c
@@ -11,18 +11,45 @@ main() {
     }
 
     TEST_CASE("Add big int") {
-        struct bigint a;
-        struct bigint b;
-        struct bigint result;
+        struct bigint a, b, result;
+
+
         set_bigint("99999999999999", &a);
         set_bigint("1", &b);
         add(&a, &b, &result);
         ASSERT_STR(get_bigint(&result), "100000000000000");
-        printf("Addition result: %s\n", get_bigint(&result));
-        printf("Expected result: 100000000000000\n");
-        printf("first number: %s\n", get_bigint(&a));
-        printf("second number: %s\n", get_bigint(&b));
+
+
+        set_bigint("99999999999999", &a);
+        set_bigint("99999999999999", &b);
+        add(&a, &b, &result);
+        ASSERT_STR(get_bigint(&result), "199999999999998");
+
+        set_bigint("0", &a);
+        set_bigint("123456789", &b);
+        add(&a, &b, &result);
+        ASSERT_STR(get_bigint(&result), "123456789");
+
+        set_bigint("123456789", &a);
+        set_bigint("0", &b);
+        add(&a, &b, &result);
+        ASSERT_STR(get_bigint(&result), "123456789");
+
+
+        set_bigint("0", &a);
+        set_bigint("0", &b);
+        add(&a, &b, &result);
+        ASSERT_STR(get_bigint(&result), "0");
+
+
+        set_bigint("12345", &a);
+        set_bigint("67890123456789", &b);
+        add(&a, &b, &result);
+        ASSERT_STR(get_bigint(&result), "67890123469134");
+
+
     }
+    
 
     END_TESTING();
 }
